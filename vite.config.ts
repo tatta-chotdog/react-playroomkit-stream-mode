766229047eaeef48594f8177a9034d8414d80015
tatta-chotdog <tatta875dev@gmail.com>
2025-04-23 11:48:05 +0900

    fix: app name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // ネットワーク全体からアクセス可能に
-    port: 5173, // デフォルトポート
+    host: true,
+    port: 5173,
   },
 });
